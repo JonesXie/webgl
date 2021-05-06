9be02a06dd6bc19995bd89a7c00565e14958eb00
Jonesxie <anhuixieqijun@163.com>
2021-05-06 17:30:21 +0800

    project init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import * as THREE from "three";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    console.log(11);
+    const canvas = document.querySelector("#canvas");
+    (canvas as HTMLCanvasElement).width = window.innerWidth;
+    (canvas as HTMLCanvasElement).height = window.innerHeight;
+    const gl = (canvas as HTMLCanvasElement).getContext("webgl");
+    gl?.clearColor(1, 0, 0, 1);
+    gl?.clear(gl?.COLOR_BUFFER_BIT);
+
+    const color = new THREE.Color("rgba(255,0,0,1)");
+    function drawColor() {
+      color.offsetHSL(0.005, 0, 0);
+      gl?.clearColor(color.r, color.g, color.b, 1);
+      gl?.clear(gl?.COLOR_BUFFER_BIT);
+      // window.requestAnimationFrame(drawColor);
+    }
+    drawColor();
+  }, []);
+  return <canvas id="canvas"></canvas>;
 }
 
 export default App;
